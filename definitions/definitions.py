d41dae35c39b53a3292043f5e61fb5b8616e72cb
fernandoareias <nando.calheirosx@gmail.com>
2021-09-18 15:33:25 -0300

    Definições do dispositivo 1

---
+++ definitions/definitions.py
@@ -0,0 +1,10 @@
+# Usuario e senha do broker
+user = ""
+password = ""
+# ID do cliente
+cliente_id = ""
+# Servidor do broker
+server = "mqtt.mydevices.com"
+# 1883 normalmente é a porta padrão do MQTT nos brokers
+port = 1883
+
